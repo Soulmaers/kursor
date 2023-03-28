3bfc11e96871724f0a928b705a7a83c696006958
Soulmaers <certiss@ya.ru>
2023-03-28 15:19:13 +0300

    !

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const app = require('./app')
-<<<<<<< HEAD
-const port = process.env.PORT || 3333
-=======
+
 require('dotenv').config();
 const port = process.env.PORT
->>>>>>> 86a9285ca1140ba6d2dcc8b1df4e89489075dce1
+
 app.listen(port, () => console.log(`Сервер запущен, порт:${port}`))
 
 
