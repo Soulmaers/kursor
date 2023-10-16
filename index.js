2152546d66576bb0c818a9ff010e87f97e88a421
Soulmaers <certiss@ya.ru>
2023-10-16 19:25:33 +0300

    !

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ const initServer = () => {
 async function init() {
     await initServer()
     await wialon()
-    // globalstart.test()
-    //  globalstart.hunterTime()
-    //  setInterval(globalstart.test, 300000)
-    // setInterval(globalstart.hunterTime, 50000)
+    globalstart.test()
+    globalstart.hunterTime()
+    setInterval(globalstart.test, 300000)
+    setInterval(globalstart.hunterTime, 50000)
     console.log('сессия открыта')
 }
 init()
