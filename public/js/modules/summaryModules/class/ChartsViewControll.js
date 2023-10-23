5711dc6754f76e5dba3ebd47f069069a3649dc94
Soulmaers <certiss@ya.ru>
2023-10-23 12:18:55 +0300

    графики привязал к списку слева

--- public/js/modules/summaryModules/class/ChartsViewControll.js
+++ public/js/modules/summaryModules/class/ChartsViewControll.js
@@ -53,70 +53,57 @@ export class ChartsViewControll {
 
     createChart(data, nameChart) {
         console.log(data)
-        // const xDate = data.find(el => el.data.length === 7)
         console.log(nameChart)
+
         const char = document.querySelector('.chart_global')
         if (char) {
             char.remove()
         }
+
         const content_lower_charts = document.querySelector('.content_lower_charts')
         const graf = document.createElement('div')
         graf.classList.add('chart_global')
         content_lower_charts.appendChild(graf)
-        // Создание SVG-элемента для отображения графика
+
         const svg = d3.select(".chart_global")
             .append("svg")
             .attr("width", 1400)
             .attr("height", 400);
 
-        // Формирование оси x с использованием дат
         const xScale = d3.scaleBand()
             .domain(data.map(function (d) { return d.date; }))
             .range([70, 1300])
-            .padding(0.1)
-            .paddingInner(1)
-            .paddingOuter(0)
-        //    .align(0)
-        // Формирование оси y для отображения значений probeg
+        //    .padding(0.1)
+        //   .paddingInner(10)
+        //  .paddingOuter(0)
+
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data.map(function (d) { return d[nameChart]; }))])
+            .domain([0, d3.max(data, function (d) { return d[nameChart]; })])
             .range([350, 20]);
 
-        // Отображение оси x
         svg.append("g")
             .attr("transform", `translate(0, ${yScale(0)})`)
             .call(d3.axisBottom(xScale));
 
-        // Отображение оси y
         svg.append("g")
             .attr("transform", `translate(70, 0)`)
             .call(d3.axisLeft(yScale));
-        // Создание линии для текущего объекта
-        const line = d3.line()
-            .x(d => xScale(d.date) + xScale.bandwidth() / 2)
-            .y(d => yScale(d[nameChart]));
 
-        // Отображение линии
-        svg.append("path")
-            .datum(data)
+        // Create bar chart by adding a rectangle for each data point
+        svg.selectAll('rect')
+            .data(data)
+            .enter().append('rect')
+            .attr("x", d => xScale(d.date))
+            .attr("y", d => yScale(d[nameChart]))
+            .attr("height", d => 350 - yScale(d[nameChart])) //substract yScale from chart height to flip chart
+            .attr("width", xScale.bandwidth()) //1300 / data.length)
             .attr("fill", "none")
-            .attr("stroke", 'blue')// colorScale(obj.idw))
-            .attr("stroke-width", 1)
-            .attr("d", line);
-
-        // Отображение линии
-        svg.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", 'blue')// colorScale(obj.idw))
-            .attr("stroke-width", 1)
-            .attr("d", line);
-
-        // Добавление подписей значений
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 0.5)
         data.forEach(function (d) {
             svg.append("text")
                 .attr("x", xScale(d.date) + xScale.bandwidth() / 2)
-                .attr("y", yScale(d[nameChart]) - 10) // сдвиг на 10 пикселей вверх, чтобы текст не пересекался с точкой данных
+                .attr("y", yScale(d[nameChart]) - 10)
                 .text(d[nameChart])
                 .attr("font-size", "10px")
                 .attr("text-anchor", "middle");
